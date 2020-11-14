58e10d493b6fb63eac4cd513ba7d987a16fba40c
MINXUAN LIU <realnimiliu@gmail.com>
2020-11-14 01:16:31 -0800

    parstagram app finished

---
+++ Parstagram/CameraViewController.swift
@@ -0,0 +1,81 @@
+//
+//  CameraViewController.swift
+//  Parstagram
+//
+//  Created by Steven Liu on 11/13/20.
+//
+
+import UIKit
+import AlamofireImage
+import Parse
+
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var commentTextField: UITextField!
+    
+    @IBAction func onSubmitButton(_ sender: Any) {
+        let post = PFObject(className: "Posts")
+        
+        post["caption"] = commentTextField.text
+        post["author"] = PFUser.current()!
+        
+        let imageData = imageView.image!.pngData()
+        let file = PFFileObject(name: "image.png", data: imageData!)
+//        let file = PFFileObject(data: imageData!)
+        
+        post["image"] = file
+        
+        post.saveInBackground { (success: Bool, error) in
+            if success {
+                self.dismiss(animated: true, completion: nil)
+                print("Photo saved!")
+            }else{
+                print("Error occurred in trying to save this photo!")
+            }
+        }
+    }
+    
+    @IBAction func onCameraButton(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        }else{
+            picker.sourceType = .photoLibrary
+        }
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
+    // Put the edited image onto the imageView placeholder
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+        
+        let size = CGSize(width: 300, height: 300)
+        let scaledImage = image.af_imageScaled(to: size)
+        imageView.image = scaledImage
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Podfile
@@ -0,0 +1,26 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'Parstagram' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Parstagram
+  pod 'Parse'
+  # This pulls in the latest version of Alamofire >= 4.4 but less than 5.0.
+  pod 'Alamofire', '~> 4.4'
+  pod 'AlamofireImage', '~> 3.1'
+  # Add additional dependencies
+  #pod 'OAuthSwift', '~> 1.1.0'
+  #pod 'OAuthSwiftAlamofire'
+
+  target 'ParstagramTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'ParstagramUITests' do
+    # Pods for testing
+  end
+
+end
